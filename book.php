55824a1bde99eca4fad43b9635db97bdc791f920
ImamAlfariziSyahputra <imam.alfarizi.777@gmail.com>
2022-04-26 23:53:58 +0700

    books: fix pagination, search feature

--- book.php
+++ book.php
@@ -1,31 +1,40 @@
 <?php
 session_start();
 
-if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
+if (!empty($_SESSION['id']) && !empty($_SESSION['email'])) {
   require_once __DIR__ . '/db-config.php';
 
-  $perPage = 1;
+  $search = !empty($_POST['search']) ? $_POST['search'] : '';
 
-  // $sql = "SELECT * FROM books ORDER BY id DESC";
-  $sql = "SELECT count(*) FROM books";
-  $statement = $connection->query($sql);
+  $searchQuery = "WHERE judul LIKE :search OR kategori LIKE :search OR pengarang LIKE :search OR penerbit LIKE :search OR status LIKE :search";
 
-  // Calculate Total pages
-  $total_results = $statement->fetchColumn();
-  $total_pages = ceil($total_results / $perPage);
+  $perPage = 5;
 
-  // Current page
-  $page = isset($_GET['page']) ? $_GET['page'] : 1;
-  $starting_limit = ($page - 1) * $perPage;
+  $sql = "SELECT count(*) FROM books $searchQuery";
+  $statement = $connection->prepare($sql);
+  $statement->execute([':search' => "%$search%"]);
 
-  // Query to fetch books
-  $sql = 'SELECT * FROM books ORDER BY id DESC LIMIT';
-  $query = "$sql $starting_limit,$perPage";
+  //* Calculate Total pages
+  $totalResults = $statement->fetchColumn();
+  //! if no result set Total Page to 1 (not Zero)
+  $totalPages = ceil($totalResults / $perPage) != 0 ? ceil($totalResults / $perPage) : 1;
 
-  $books = $connection->query($query)->fetchAll();
+  //* Current page
+  //! Jika page kurang dari 1 / kosong, Set "$currentPage" to 1
+  $currentPage = empty($_POST['page']) || $_POST['page'] < 1 ? 1 : $_POST['page'];
 
-  // var_dump($books);
-  // exit();
+  // var_dump($currentPage);
+  // var_dump($totalPages);
+
+  $startingLimits = ($currentPage - 1) * $perPage;
+
+  //* Query to fetch books
+  $sql = "SELECT * FROM books $searchQuery ORDER BY id DESC LIMIT $startingLimits,$perPage";
+  $statement = $connection->prepare($sql);
+  $statement->execute([':search' => "%$search%"]);
+
+  $books = $statement->fetchAll();
+  $rows = $statement->rowCount();
 } else {
   $alert = <<<ALERT
     <script>
@@ -59,28 +68,32 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
 
     <!-- Add, Print Button -->
     <div class="flex items-center justify-end space-x-3">
+      <a href='' class="flex space-x-2 items-center bg-green-500 hover:bg-green-800 text-white p-2.5 px-4 rounded-lg shadow-md hover:shadow-none font-bold mb-4">
+        <img src="icons/refresh-w.svg" alt="Refresh Logo" class="w-4 h-4">
+        <span>Refresh</span>
+      </a>
       <button type='button' onclick="printAll();" class="flex space-x-2 items-center bg-yellow-500 hover:bg-yellow-800 text-white p-2.5 px-4 rounded-lg shadow-md hover:shadow-none font-bold mb-4">
-        <img src="icons/print-w.svg" alt="" class="w-4">
+        <img src="icons/print-w.svg" alt="Print Logo" class="w-4 h-4">
         <span>Cetak</span>
       </button>
       <a href='book-add.php' class="flex space-x-2 items-center bg-blue-500 hover:bg-blue-800 text-white p-2.5 px-4 rounded-lg shadow-md hover:shadow-none font-bold mb-4">
-        <img src="icons/plus-w.svg" alt="" class="w-4">
+        <img src="icons/plus-w.svg" alt="Plus Logo" class="w-4 h-4">
         <span>Tambah</span>
       </a>
     </div>
     <!-- END Add, Print Button -->
 
     <!-- Search Input -->
-    <form class="flex items-center">
-      <label for="simple-search" class="sr-only">Search</label>
+    <form action="book.php" method="POST" class="flex items-center">
+      <label for="search" class="sr-only">Search</label>
       <div class="relative w-full">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <img src="icons/search-gray.svg" alt="" class="h-5 w-5">
+          <img src="icons/search-gray.svg" alt="Search Icon" class="h-5 w-5">
         </div>
-        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-purple-400 block w-full pl-10 p-2.5" placeholder="Search" required>
+        <input type="text" name='search' id="search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-purple-400 block w-full pl-10 p-2.5" placeholder="Search" value='<?= !empty($_POST['search']) ? $_POST['search'] : '' ?>'>
       </div>
       <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-purple-700 rounded-lg border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300">
-        <img src="icons/search-w.svg" alt="" class="h-5 w-5">
+        <img src="icons/search-w.svg" alt="Search Icon" class="h-5 w-5">
       </button>
     </form>
     <!-- END Search Input -->
@@ -116,35 +129,47 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
         </tr>
       </thead>
       <tbody>
-        <?php foreach ($books as $book) : ?>
+        <?php if ($rows == 0) : ?>
           <tr class="bg-white border-b">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <?= $book['judul'] ?>
-            </th>
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <?= $book['kategori'] ?>
-            </th>
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <?= $book['pengarang'] ?>
-            </th>
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <?= $book['penerbit'] ?>
-            </th>
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <?= $book['status'] ?>
-            </th>
-            <td class="flex justify-content-evenly items-center space-x-1.5 py-4">
-              <button type="button" onclick="printSingle(<?= $book['id'] ?>)" class="bg-yellow-500 hover:bg-yellow-600 py-1.5 px-2.5 rounded font-medium text-white">Cetak</button>
-              <span> - </span>
-              <a href="book-edit.php?id=<?= $book['id'] ?>" class="bg-green-600 hover:bg-green-700 py-1.5 px-2.5 rounded font-medium text-white">Edit</a>
-              <span> - </span>
-              <form action="logic/buku/delete.php" method="POST">
-                <input type="text" class='hidden' name='id' value='<?= $book['id'] ?>'>
-                <button type="submit" onclick="return confirm(' Apakah yakin data akan di hapus?')" class="block bg-red-600 hover:bg-red-700 py-1.5 px-2.5 rounded font-medium text-white">Delete</button>
-              </form>
-            </td>
+            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-center" colspan="6">Data Tidak Ditemukan.</td>
           </tr>
-        <?php endforeach; ?>
+        <?php else : ?>
+          <?php foreach ($books as $book) : ?>
+            <tr class="bg-white border-b">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <?= $book['judul'] ?>
+              </th>
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <?= $book['kategori'] ?>
+              </th>
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <?= $book['pengarang'] ?>
+              </th>
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <?= $book['penerbit'] ?>
+              </th>
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <?= $book['status'] ?>
+              </th>
+              <td class="flex justify-content-evenly items-center space-x-1.5 py-4 px-6">
+                <button type="button" onclick="printSingle(<?= $book['id'] ?>)" class="bg-yellow-500 hover:bg-yellow-600 p-2.5 rounded font-medium text-white">
+                  <img src="icons/print-w.svg" alt="Print Logo" class="w-5 h-5">
+                </button>
+                <span> - </span>
+                <a href="book-edit.php?id=<?= $book['id'] ?>" class="bg-green-600 hover:bg-green-700 p-2.5 rounded font-medium text-white">
+                  <img src="icons/edit-w.svg" alt="Edit Logo" class="w-5 h-5">
+                </a>
+                <span> - </span>
+                <form action="logic/buku/delete.php" method="POST" class='m-0'>
+                  <input type="text" class='hidden' name='id' value='<?= $book['id'] ?>'>
+                  <button type="submit" onclick="return confirm(' Apakah yakin data akan di hapus?')" class="block bg-red-600 hover:bg-red-700 p-2.5 rounded font-medium text-white">
+                    <img src="icons/trash-w.svg" alt="Remove Logo" class="w-5 h-5">
+                  </button>
+                </form>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        <?php endif; ?>
       </tbody>
     </table>
 
@@ -155,25 +180,47 @@ if (isset($_SESSION['id']) && isset($_SESSION['email'])) {
   <div class="flex items-center justify-between">
     <!-- Help text -->
     <span class="text-sm text-gray-700">
-      Jumlah Data : <span class="font-semibold text-gray-900"><?= $total_results ?></span>
+      Jumlah Data : <span class="font-semibold text-gray-900"><?= $totalResults ?></span>
     </span>
     <!-- Buttons -->
     <div class="inline-flex mt-2 xs:mt-0">
-      <?php $previousPage = $_GET['page'] - 1 ?>
-      <a href="<?= "?page=$previousPage" ?>" class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $_GET['page'] == 1 ? '!hidden' : '' ?>">
-        Previous
-      </a>
-      <?php for ($page = 1; $page <= $total_pages; $page++) : ?>
-        <a href="<?= "?page=$page" ?>" class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $page == $_GET['page'] ? 'bg-blue-400 !text-white border-blue-400 hover:bg-blue-500 hover:text-white' : '' ?>">
-          <?= $page; ?>
-        </a>
+      <?php $previousPage = $currentPage - 1 ?>
+      <?php $nextPage = $currentPage + 1 ?>
+
+      <form action="" method="POST" class="inline-block">
+        <?php if (!empty($_POST['search'])) : ?>
+          <input type="text" name='search' class="hidden" value="<?= $_POST['search'] ?>">
+        <?php endif; ?>
+        <input type="text" name='page' class="hidden" value="<?= $previousPage ?>">
+        <button type="submit" class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $currentPage == 1 ? '!hidden' : '' ?>">
+          Previous
+        </button>
+      </form>
+
+      <?php for ($pageNumber = 1; $pageNumber <= $totalPages; $pageNumber++) : ?>
+        <form action="" method="POST" class="inline-block">
+          <?php if (!empty($_POST['search'])) : ?>
+            <input type="text" name='search' class="hidden" value="<?= $_POST['search'] ?>">
+          <?php endif; ?>
+          <input type="text" name='page' class="hidden" value="<?= $pageNumber ?>">
+          <button type="submit" class="py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $pageNumber == $currentPage ? 'bg-blue-400 !text-white border-blue-400 hover:bg-blue-500 hover:text-white' : '' ?>">
+            <?= $pageNumber; ?>
+          </button>
+        </form>
       <?php endfor; ?>
-      <?php $nextPage = $_GET['page'] + 1 ?>
-      <a href="<?= "?page=$nextPage" ?>" class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $_GET['page'] == $total_pages ? '!hidden' : '' ?>">Next</a>
+
+      <form action="" method="POST" class="inline-block">
+        <?php if (!empty($_POST['search'])) : ?>
+          <input type="text" name='search' class="hidden" value="<?= $_POST['search'] ?>">
+        <?php endif; ?>
+        <input type="text" name='page' class="hidden" value="<?= $nextPage ?>">
+        <button type="submit" class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $currentPage == $totalPages ? '!hidden' : '' ?>">Next</button>
+      </form>
+
     </div>
+
   </div>
   <!-- END Pagination -->
-
 
 </div>
 <!-- END Content -->
